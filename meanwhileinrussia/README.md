f378d03c2afbc41d09c5b35ff6ae208a292472f3
Felix Mueller <felix@s4ku.com>
2012-08-17 04:24:05 +0200

    added meanwhileinrussia plugin

---
+++ meanwhileinrussia/README.md
@@ -0,0 +1,7 @@
+# Meanwhile in Russia plugin
+
+This is just a stupid fun plugin for sevabot to parse random lines from a URL which gives us fresh videos from mother Russia <3
+
+Pull requests for submitting new "meanwhile in Russia" videos can be submitted here https://github.com/lixef/glowing-octo-wight/tree/gh-pages
+
+The default URL is hosted on GitHub pages.
---
+++ meanwhileinrussia/meanwhileinrussia.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+'''
+This is just a stupid fun plugin for sevabot to parse random lines from a URL which gives us fresh videos from mother Russia <3
+Pull requests for submitting new "meanwhile in Russia" videos can be submitted here https://github.com/lixef/glowing-octo-wight/tree/gh-pages
+The default URL is hosted on GitHub pages.
+'''
+
+import urllib
+import random
+
+url = urllib.urlopen('http://felix.tl/glowing-octo-wight/').read().splitlines()
+myline = random.choice(url)
+print(myline)
