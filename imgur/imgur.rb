ff582e02e3d085679440374702ef6ab07fa6ec6a
Felix Mueller <felix@s4ku.com>
2012-09-08 20:26:59 +0200

    new file:   imgur/imgur.rb

---
+++ imgur/imgur.rb
@@ -0,0 +1,8 @@
+require 'json'
+require 'open-uri'
+
+gallery = JSON.parse(open('http://imgur.com/gallery.json').read)['gallery']
+
+gallery.each {|i| i['url'] = "http://imgur.com/#{i['hash']}#{i['ext']}" }
+
+puts gallery.sample['url']
